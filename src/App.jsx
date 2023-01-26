38dfee2c96a1527ec3378546ed19a04fef75deb8
natashasilveira <natashasilveira.p@gmail.com>
2023-01-26 00:40:57 -0300

    Página fusca - aula 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import fusca from './fusca.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={fusca} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Fusca Vermelho
         </p>
         <a
           className="App-link"
