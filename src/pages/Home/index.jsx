9c35b3f2182a22772e6aaefa47080710de7ac942
natashasilveira <natashasilveira.p@gmail.com>
2023-01-28 02:14:52 -0300

    Ajuste página Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
+import Title from "../../components/Title";
 
 function Home() {
 
   const navigate = useNavigate();
 
   return (
-    <>
-      <h1>Home</h1>
+    <div className="container">
+      <Title text="Home" />
       <Button
         btnName="Voltar"
         redirect={() => navigate("/")}
       />
-    </>
+    </div>
   );
 }
 
